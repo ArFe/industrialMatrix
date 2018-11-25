83650add26722dacf108a47326f5e7cb7a4359a2
ArFe <ArFe@users.noreply.github.com>
2018-11-25 18:40:12 -0500

    first commit

--- index.js
+++ index.js
@@ -10,8 +10,27 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log(msg);
   });
 });
+
+// setup a GET 'route' to listen on /banner
+app.get("/banner", function(req,res){
+  console.log(req.query);
+  let filter = JSON.stringify(req.query);
+  //let filter = {"departmentName": req.params.value };
+  res.send("<html><head><title>HTTP Push Ack</title></head><body>id=" + req.query.id + "</body></html>");
+  io.emit('chat message', filter);
+
+});
+
+// setup a POST 'route' to listen on /banner
+app.post("/banner", function(req,res){
+  console.log(req.body);
+  io.emit('chat message', req.body);
+  res.send("<html><head><title>HTTP Push Ack</title></head><body>id=244b9fb1-7085-4877-8352-994f7b632b4f</body></html>");
+});
+
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
