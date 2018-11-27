1e6b79ef2a750bd59be7a2b1b8558cae790da3a4
ArFe <ArFe@users.noreply.github.com>
2018-11-26 22:36:05 -0500

    Add status page

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require("express");
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require("path");
 var port = process.env.PORT || 3000;
+
+app.use(express.static("./public/"));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/status', function(req, res){
+  res.sendFile(__dirname + '/status.html');
 });
 
 io.on('connection', function(socket){
---
+++ public/script.js
@@ -0,0 +1,15 @@
+const socket = io();
+let test = true; 
+socket.on('chat message', function(msg){
+    console.log(msg);
+    console.log("Test = " + test)
+    obj = JSON.parse(msg);
+    let elem = document.querySelector("#led");
+    if(obj["reg1"] = 1)
+        elem.classList.add("red");
+    else
+        elem.classList.remove("red");
+
+    test = !test;
+
+});
