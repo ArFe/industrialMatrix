154473adaa60a041db1fcf2f21c1251c6ab2b483
ArFe <ArFe@users.noreply.github.com>
2019-01-18 13:47:03 -0500

    fix script

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
 const socket = io();
-let test = true; 
 let nodeNum;
 let dxmId;
 
@@ -19,7 +18,6 @@ function startSS(){
 }
 socket.on('chat message', function(msg){
     console.log(msg);
-    console.log("Test = " + test)
     obj = JSON.parse(JSON.stringify(msg));
     let elem = document.querySelector("#led");
     if (elem != null) {
@@ -28,53 +26,55 @@ socket.on('chat message', function(msg){
         else
             elem.classList.remove("red");
     }
-    test = !test;
 
-    dxmId = obj["id"];
-    nodeNum = obj["reg6"];
-    elem = document.querySelector("#"+dxmId);
-    if (elem != null) {
+    if(obj != null){
+        dxmId = obj["id"];
+        nodeNum = obj["reg6"];
+        console.log("dxmID = " + dxmId)
+        elem = document.querySelector("#"+dxmId);
+        console.log("elem = " + elem)
+        if (elem != null) {
 
-        var c = document.getElementById("myCanvas");
-        var ctx = c.getContext("2d");
+            var c = document.getElementById("myCanvas");
+            var ctx = c.getContext("2d");
 
-        c.style.position = 'fixed';
-        c.style.top= 0;
-        c.style.left= 0;
-        c.style.zIndex= -1;
+            c.style.position = 'fixed';
+            c.style.top= 0;
+            c.style.left= 0;
+            c.style.zIndex= -1;
 
-        ctx.canvas.width  = window.innerWidth-10;
-        ctx.canvas.height = window.innerHeight-10;
+            ctx.canvas.width  = window.innerWidth-10;
+            ctx.canvas.height = window.innerHeight-10;
 
-        
-        ctx.beginPath();
-        ctx.rect(10, window.innerHeight/8, window.innerWidth*(obj["reg1"]/100), window.innerHeight/9);
-        ctx.fillStyle = "green";
-        ctx.fill();
+            
+            ctx.beginPath();
+            ctx.rect(10, window.innerHeight/8, window.innerWidth*(obj["reg1"]/100), window.innerHeight/9);
+            ctx.fillStyle = "green";
+            ctx.fill();
 
-        ctx.beginPath();
-        ctx.rect(10, 1*(window.innerHeight/8)+ window.innerHeight/8, window.innerWidth*(obj["reg2"]/100), window.innerHeight/9);
-        ctx.fillStyle = "yellow";
-        ctx.fill();
+            ctx.beginPath();
+            ctx.rect(10, 1*(window.innerHeight/8)+ window.innerHeight/8, window.innerWidth*(obj["reg2"]/100), window.innerHeight/9);
+            ctx.fillStyle = "yellow";
+            ctx.fill();
 
-        ctx.beginPath();
-        ctx.rect(10, 2*(window.innerHeight/8)+ window.innerHeight/8, window.innerWidth*(obj["reg3"]/100), window.innerHeight/9);
-        ctx.fillStyle = "red";
-        ctx.fill();
-        
-        ctx.beginPath();
-        ctx.rect(10, 3*(window.innerHeight/8)+ window.innerHeight/8, window.innerWidth*(obj["reg4"]/100), window.innerHeight/9);
-        ctx.fillStyle = "gray";
-        ctx.fill();
+            ctx.beginPath();
+            ctx.rect(10, 2*(window.innerHeight/8)+ window.innerHeight/8, window.innerWidth*(obj["reg3"]/100), window.innerHeight/9);
+            ctx.fillStyle = "red";
+            ctx.fill();
+            
+            ctx.beginPath();
+            ctx.rect(10, 3*(window.innerHeight/8)+ window.innerHeight/8, window.innerWidth*(obj["reg4"]/100), window.innerHeight/9);
+            ctx.fillStyle = "gray";
+            ctx.fill();
 
-        ctx.font = "4em Arial";
-        ctx.fillStyle = "black";
-        ctx.textAlign = "left";
-        ctx.fillText("Node " + nodeNum, 15, window.innerHeight/12); 
-        ctx.fillText("Excelent " + obj["reg1"] + "%", 15, 1*(window.innerHeight/8) + window.innerHeight/12); 
-        ctx.fillText("Good " + obj["reg2"] + "%", 15, 2*(window.innerHeight/8) + window.innerHeight/12); 
-        ctx.fillText("Marginal " + obj["reg3"] + "%", 15, 3*(window.innerHeight/8) + window.innerHeight/12); 
-        ctx.fillText("Missed " + obj["reg4"] + "%", 15, 4*(window.innerHeight/8) + window.innerHeight/12); 
+            ctx.font = "4em Arial";
+            ctx.fillStyle = "black";
+            ctx.textAlign = "left";
+            ctx.fillText("Node " + nodeNum, 15, window.innerHeight/12); 
+            ctx.fillText("Excelent " + obj["reg1"] + "%", 15, 1*(window.innerHeight/8) + window.innerHeight/12); 
+            ctx.fillText("Good " + obj["reg2"] + "%", 15, 2*(window.innerHeight/8) + window.innerHeight/12); 
+            ctx.fillText("Marginal " + obj["reg3"] + "%", 15, 3*(window.innerHeight/8) + window.innerHeight/12); 
+            ctx.fillText("Missed " + obj["reg4"] + "%", 15, 4*(window.innerHeight/8) + window.innerHeight/12); 
+        }
     }
-
 });
